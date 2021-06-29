6ec7def44742177dde430fcc9b228e6a45c26d58
Brian Gates <briandotgates@gmail.com>
2021-06-28 20:32:31 -0400

    move swedes and muscovites

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import "./App.css";
 import * as A from "fp-ts/lib/Array";
 import { castle, eqPosition, setupPieces } from "./setupBoard";
@@ -12,6 +15,17 @@ const App = () => {
   const [pieces, setPieces] = useState<Array<Piece>>(setupPieces());
   const [availableSpaces, setAvailableSpaces] = useState<Array<Position>>([]);
 
+  const movePiece = (from: Position, to: Position) => {
+    setPieces(
+      pipe(
+        pieces,
+        A.map((p) =>
+          eqPosition.equals(p.position, from) ? { ...p, position: to } : p
+        )
+      )
+    );
+    setCurrentPlayer(currentPlayer === "attacker" ? "defender" : "attacker");
+  };
   const setAvailableSpacesFor = (p: Position) => () =>
     pipe(
       getAvailableSpaces(
@@ -31,35 +45,43 @@ const App = () => {
         <h1>Hnefatafl</h1>
       </header>
       <div className="grid place-content-center">
-        <div className="flex">
-          {pipe(
-            A.range(0, 8),
-            A.map((row) => (
-              <div className="flex-col">
-                {pipe(
-                  A.range(0, 8),
-                  A.map((col) => (
-                    <Space
-                      available={pipe(
-                        availableSpaces,
-                        A.elem(eqPosition)({ row, col })
-                      )}
-                      occupant={pipe(
-                        pieces,
-                        A.findFirst((piece) =>
-                          eqPosition.equals(piece.position, { row, col })
-                        )
-                      )}
-                      isCastle={eqPosition.equals(castle, { row, col })}
-                      onHover={setAvailableSpacesFor({ row, col })}
-                      onLeave={resetAvailableSpaces}
-                    />
-                  ))
-                )}
-              </div>
-            ))
-          )}
-        </div>
+        <div>{currentPlayer}'s Turn</div>
+        <DndProvider backend={HTML5Backend}>
+          <div className="flex">
+            {pipe(
+              A.range(0, 8),
+              A.map((row) => (
+                <div className="flex-col" key={row}>
+                  {pipe(
+                    A.range(0, 8),
+                    A.map((col) => (
+                      <Space
+                        key={col}
+                        available={pipe(
+                          availableSpaces,
+                          A.elem(eqPosition)({ row, col })
+                        )}
+                        occupant={pipe(
+                          pieces,
+                          A.findFirst((piece) =>
+                            eqPosition.equals(piece.position, { row, col })
+                          )
+                        )}
+                        isCastle={eqPosition.equals(castle, { row, col })}
+                        onHover={setAvailableSpacesFor({ row, col })}
+                        onLeave={resetAvailableSpaces}
+                        onMove={movePiece}
+                        currentPlayer={currentPlayer}
+                        row={row}
+                        col={col}
+                      />
+                    ))
+                  )}
+                </div>
+              ))
+            )}
+          </div>
+        </DndProvider>
       </div>
     </div>
   );
--- src/Space.tsx
+++ src/Space.tsx
@@ -2,56 +2,113 @@ import React from "react";
 import { constVoid, flow, pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import * as M from "pattern-matching-ts/lib/match";
-import { Piece } from "./types";
+import { Piece, Position, Side } from "./types";
+import { useDrag, useDrop } from "react-dnd";
+
+const GenericPiece = ({
+  row,
+  col,
+  type,
+  depiction,
+}: {
+  row: number;
+  col: number;
+  type: string;
+  depiction: string;
+}) =>
+  pipe(
+    useDrag(() => ({
+      type,
+      item: { row, col },
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    })),
+    ([{ isDragging }, drag]) => (
+      <div
+        ref={drag}
+        className={`w-8 h-8 grid display-content-center opacity-${
+          isDragging ? 25 : 100
+        }`}
+      >
+        {depiction}
+      </div>
+    )
+  );
+
+const Swede = ({ row, col }: { row: number; col: number }) => (
+  <GenericPiece row={row} col={col} type="swede" depiction="S" />
+);
+const Muscovite = ({ row, col }: { row: number; col: number }) => (
+  <GenericPiece row={row} col={col} type="muscovite" depiction="M" />
+);
 export const Space = ({
   available,
   occupant,
   isCastle,
   onHover,
   onLeave,
+  onMove,
+  row,
+  col,
+  currentPlayer,
 }: {
   available: boolean;
   occupant: O.Option<Piece>;
   isCastle: boolean;
+  row: number;
+  col: number;
+  currentPlayer: Side;
   onHover: () => void;
   onLeave: () => void;
-}) => (
-  <div
-    className={
-      isCastle
-        ? "bg-red-500 text-white border"
-        : available
-        ? "bg-blue-200 border"
-        : "border"
-    }
-    onMouseEnter={pipe(
-      occupant,
-      O.fold(
-        () => constVoid,
-        () => onHover
-      )
-    )}
-    onMouseLeave={onLeave}
-  >
-    {pipe(
-      occupant,
-      O.fold(
-        () => <div className="w-8 h-8"></div>,
-        flow(
-          M.match({
-            king: () => (
-              <div className="w-8 h-8 grid display-content-center">K</div>
-            ),
-            swede: () => (
-              <div className="w-8 h-8 grid display-content-center">S</div>
-            ),
-            muscovite: () => (
-              <div className="w-8 h-8 grid display-content-center">M</div>
-            ),
-            _: () => <div />,
-          })
+  onMove: (from: Position, to: Position) => void;
+}) => {
+  const [{ isOver }, drop] = useDrop(
+    () => ({
+      accept: currentPlayer === "attacker" ? "muscovite" : "swede",
+      drop: (item, monitor) => {
+        onMove(monitor.getItem(), { row, col });
+      },
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+    }),
+    [row, col, currentPlayer]
+  );
+  return (
+    <div
+      className={
+        isCastle
+          ? "bg-red-500 text-white border"
+          : available
+          ? "bg-blue-200 border"
+          : "border"
+      }
+      onMouseEnter={pipe(
+        occupant,
+        O.fold(
+          () => constVoid,
+          () => onHover
         )
-      )
-    )}
-  </div>
-);
+      )}
+      onMouseLeave={onLeave}
+    >
+      {pipe(
+        occupant,
+        O.fold(
+          () => <div className="w-8 h-8" ref={drop}></div>,
+          flow(
+            M.match({
+              king: () => (
+                <div className="w-8 h-8 grid display-content-center">K</div>
+              ),
+              swede: () => <Swede row={row} col={col} />,
+              muscovite: () => <Muscovite row={row} col={col} />,
+              _: () => <div />,
+            })
+          )
+        )
+      )}
+    </div>
+  );
+};
