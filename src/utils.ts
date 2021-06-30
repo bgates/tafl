e1589c24e8d92ba7d9c660a3110fc483aec84b11
Brian Gates <briandotgates@gmail.com>
2021-06-30 19:34:25 -0400

    start king+castle capture rules

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,14 @@ import { Ord } from "fp-ts/lib/Ord";
 import * as O from "fp-ts/lib/Option";
 import * as R from "fp-ts/lib/Record";
 import { ordCol, ordRow, Piece, Position, Side } from "./types";
-import { eqPosition } from "./setupBoard";
+import { castle, eqPosition } from "./setupBoard";
 
+const positionsAdjacentToCastle = [
+  { ...castle, row: castle.row + 1 },
+  { ...castle, row: castle.row - 1 },
+  { ...castle, col: castle.col + 1 },
+  { ...castle, col: castle.col - 1 },
+];
 const barriers =
   <A>(ord: Ord<A>) =>
   ({
@@ -88,11 +94,14 @@ const isOtherSide = (side: Side) => (piece: Piece) =>
   side === "attacker" ? piece._tag !== "muscovite" : piece._tag === "muscovite";
 const isSameSide = (side: Side) => (piece: Piece) => !isOtherSide(side)(piece);
 type BoardSides = "left" | "right" | "top" | "bottom";
-type PiecesInPosition = Record<BoardSides, O.Option<Piece>>;
+type PiecesInPosition = Record<BoardSides, O.Option<PieceOrCastle>>;
+type PieceOrCastle = {
+  position: Position;
+};
 
 const piecesInRelativePosition = (
   position: Position,
-  pieces: Array<Piece>,
+  pieces: Array<PieceOrCastle>,
   n: number
 ): PiecesInPosition =>
   pipe(pieces, (pieces) => ({
@@ -135,10 +144,11 @@ const piecesInRelativePosition = (
   }));
 
 const alliesInPositionToCapture =
-  (position: Position) => (pieces: Array<Piece>) =>
+  (position: Position) => (pieces: Array<PieceOrCastle>) =>
     piecesInRelativePosition(position, pieces, 2);
-const adjacentEnemies = (position: Position) => (pieces: Array<Piece>) =>
-  piecesInRelativePosition(position, pieces, 1);
+const adjacentEnemies =
+  (position: Position) => (pieces: Array<PieceOrCastle>) =>
+    piecesInRelativePosition(position, pieces, 1);
 
 const capturableEnemies =
   (allies: PiecesInPosition) => (enemies: PiecesInPosition) => ({
@@ -159,6 +169,90 @@ const capturableEnemies =
       O.chain((a) => enemies.right)
     ),
   });
+// if the king is not in the castle, it can help to capture enemies
+const isKing = (piece: Piece) => piece._tag === "king";
+const isPiece = (p: Piece | { position: Position }): p is Piece => "_tag" in p;
+
+const alliesAndEmptyCastle = (pieces: Array<Piece>) => (allies: Array<Piece>) =>
+  pipe(
+    pieces,
+    A.findFirst(isKing),
+    O.chain(
+      O.fromPredicate((king) => eqPosition.equals(king.position, castle))
+    ),
+    O.fold(
+      () => [{ position: castle }, ...allies],
+      () => allies
+    )
+  );
+const adjacentToThisManyAttackers = (
+  piece: Piece,
+  pieces: Array<Piece>,
+  n: number
+) =>
+  pipe(
+    piecesInRelativePosition(piece.position, pieces, 1),
+    R.compact,
+    R.collect((k, v) => v),
+    A.filter(isPiece),
+    A.filter(isSameSide("attacker")),
+    (arr) => arr.length >= n
+  );
+// If the king is on a square adjoining the castle (horizontally or vertically) he must be surrounded on the three remaining sides by his enemies.
+const kingAdjacentToCastle =
+  (pieces: Array<Piece>) => (capturablePieces: Array<Piece>) =>
+    pipe(
+      capturablePieces,
+      A.findFirst(isKing),
+      O.chain(
+        O.fromPredicate((king) =>
+          pipe(positionsAdjacentToCastle, A.elem(eqPosition)(king.position))
+        )
+      ),
+      O.fold(
+        () => capturablePieces,
+        (king) =>
+          pipe(
+            adjacentToThisManyAttackers(king, pieces, 3),
+            (trueIfKingCapturable) =>
+              pipe(
+                capturablePieces,
+                A.filter(
+                  (piece) =>
+                    trueIfKingCapturable ||
+                    !eqPosition.equals(king.position, piece.position)
+                )
+              )
+          )
+      )
+    );
+// If the king is inside the castle, he is not captured until he is surrounded on all four sides
+const kingInsideCastle =
+  (pieces: Array<Piece>) => (capturablePieces: Array<Piece>) =>
+    pipe(
+      capturablePieces,
+      A.findFirst(isKing),
+      O.chain(
+        O.fromPredicate((king) => eqPosition.equals(king.position, castle))
+      ),
+      O.fold(
+        () => capturablePieces,
+        (king) =>
+          pipe(
+            adjacentToThisManyAttackers(king, pieces, 4),
+            (trueIfKingCapturable) =>
+              pipe(
+                capturablePieces,
+                A.filter(
+                  (piece) =>
+                    trueIfKingCapturable ||
+                    !eqPosition.equals(king.position, piece.position)
+                )
+              )
+          )
+      )
+    );
+
 // normal capture is when you move a piece on your Side to a new Position and
 // it is adjacent to a piece on the other Side, and beyond that is another piece on your Side
 export const capturedPieces = (
@@ -174,9 +268,13 @@ export const capturedPieces = (
       pipe(
         pieces,
         A.filter(isSameSide(side)),
+        alliesAndEmptyCastle(pieces),
         alliesInPositionToCapture(position)
       )
     ),
     R.compact,
-    R.collect((k, v) => v)
+    R.collect((k, v) => v),
+    A.filter(isPiece),
+    kingAdjacentToCastle(pieces),
+    kingInsideCastle(pieces)
   );
